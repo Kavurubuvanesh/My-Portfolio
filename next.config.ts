d010e24248cd9cbb6ded96abe622f724df9f4b0c
Kavuru Buvanesh <169604812+Kavurubuvanesh@users.noreply.github.com>
2026-06-25 08:37:34 +0530

    Changes in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // <-- THIS IS THE ARCHITECTURE FIX
+  // ... any other config settings you have
 };
 
 export default nextConfig;
